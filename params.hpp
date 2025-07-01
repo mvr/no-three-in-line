6be1d5539c25ebdcc0f111b6ae42c9beab806d7d
mvr <mitchell.v.riley@gmail.com>
2025-07-01 09:40:25 +0400

    Bump default N to 12

--- params.hpp
+++ params.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-const unsigned N = 10;
+const unsigned N = 12;
+static_assert(N <= 64);
 
 const unsigned WARPS_PER_BLOCK = 4;
-const unsigned MAX_BATCH_SIZE = 1024;
+const unsigned MAX_BATCH_SIZE = 1<<14;
 const unsigned SOFT_BRANCH_1_THRESHOLD = 1000;
 const unsigned SOFT_BRANCH_2_THRESHOLD = 5;
