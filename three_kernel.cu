2df08bc80a6e170c5be65338ab34c51519e6b715
mvr <mitchell.v.riley@gmail.com>
2025-07-13 11:51:50 +0400

    Merge cases in `resolve_outcome_row`

--- three_kernel.cu
+++ three_kernel.cu
@@ -72,40 +72,22 @@ __device__ void resolve_outcome_row(const ThreeBoard<N, W> board, Axis axis, uns
     return (axis == Axis::Horizontal) ? cuda::std::pair<unsigned, unsigned>{c, ix} : cuda::std::pair<unsigned, unsigned>{ix, c};
   };
 
-  if (on_count == 1) {
-    for (; remaining; remaining &= remaining - 1) {
-      auto cell = make_cell(find_first_set<W>(remaining));
+  ThreeBoard<N, W> tried_board = board;
+  unsigned remaining_count = popcount<W>(remaining) - (on_count == 0 ? 1 : 0);
+  for (; remaining_count > 0; remaining_count--, remaining &= remaining - 1) {
+    auto cell = make_cell(find_first_set<W>(remaining));
 
-      ThreeBoard<N, W> sub_board = board;
-      sub_board.known_on.set(cell);
-      sub_board.eliminate_all_lines(cell);
-      sub_board.propagate();
+    ThreeBoard<N, W> sub_board = tried_board;
+    sub_board.known_on.set(cell);
+    sub_board.eliminate_all_lines(cell);
+    sub_board.propagate();
 
-      if(sub_board.consistent()) {
-        DeviceProblem<W> problem = {sub_board.known_on, sub_board.known_off};
-        stack_push(stack, problem);
-      }
+    if(sub_board.consistent()) {
+      DeviceProblem<W> problem = {sub_board.known_on, sub_board.known_off};
+      stack_push(stack, problem);
     }
-  }
 
-  if (on_count == 0) {
-    ThreeBoard<N, W> tried_board = board;
-    unsigned remaining_count = popcount<W>(remaining);
-    for (; remaining_count > 1; remaining_count--, remaining &= remaining - 1) {
-      auto cell = make_cell(find_first_set<W>(remaining));
-
-      ThreeBoard<N, W> sub_board = tried_board;
-      sub_board.known_on.set(cell);
-      sub_board.eliminate_all_lines(cell);
-      sub_board.propagate();
-
-      if(sub_board.consistent()) {
-        DeviceProblem<W> problem = {sub_board.known_on, sub_board.known_off};
-        stack_push(stack, problem);
-      }
-
-      tried_board.known_off.set(cell);
-    }
+    tried_board.known_off.set(cell);
   }
 }
 
